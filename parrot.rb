6bbb899f90b039f0a6bfa862b7280940137f1992
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-24 22:04:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string)
-  if string == " "
+def parrot(string="Squawk!")
+  if string == "Squawk!"
     puts "Squawk!"
     return "Squawk!"
   else
