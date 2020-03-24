c74c4f9d26fee8d920e1206bb7ebd82eb47668ca
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-24 22:04:14 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(string="Squawk!")
+  if string == "Squawk!"
+    puts "Squawk!"
+    return "Squawk!"
+  else
+    puts string
+    return string
+  end
+end
