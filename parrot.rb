8d10a94ea969113aa7c8e443a2554c0b14448409
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-24 22:02:02 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(string)
+  if string = " "
+    puts "Squawk!"
+    return "Squawk!"
+  else
+    puts string
+    return string
+  end
+end
